9376c9fae43336bb69b45fd46d5e9428f7d1ffe8
Miguel <miguel.perez.pasalodos@gmail.com>
2018-06-21 11:33:33 +0200

    Add Analytics tracking code

--- src/main/scala/templates/LayoutT.scala
+++ src/main/scala/templates/LayoutT.scala
@@ -5,13 +5,28 @@ import scalatags.Text.{TypedTag, tags2}
 
 /** Base layout for all pages */
 object LayoutT {
+
+  /** Analytics tracking code */
+  private val analytics = raw(
+    """
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121170824-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-121170824-1');
+    </script>
+    """
+  )
+
   def apply(options: LayoutOptions)(content: TypedTag[String]*): TypedTag[String] = html(
     head(
       meta(charset := "UTF-8"),
       link(rel := "stylesheet", href := "/assets/styles/style.css"),
       link(rel := "stylesheet", href := "https://fonts.googleapis.com/css?family=Open+Sans"),
       link(rel := "stylesheet", href := "https://use.fontawesome.com/releases/v5.0.13/css/all.css"),
-      tags2.title(options.title)
+      tags2.title(options.title),
+      analytics
     ),
     body(content)
   )
