b3c1e37fa68190e39bc2fd63bda18460dd81fb31
Miguel <miguel.perez.pasalodos@gmail.com>
2018-07-06 09:11:29 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+stylus src/main/resources/styles --out src/main/resources/public/styles -u nib -c -r --compress
+sbt clean universal:packageBin
+
+echo "Done, you can copy to production: ./target/universal/mpasa-0.1.0.zip"
