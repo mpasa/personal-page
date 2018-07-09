8824e0abdc503696f9ff80ea3312d44e3a92750b
Miguel <miguel.perez.pasalodos@gmail.com>
2018-07-09 18:18:50 +0200

    Add a RSS feed button to the header

--- src/main/scala/templates/components/SocialT.scala
+++ src/main/scala/templates/components/SocialT.scala
@@ -1,5 +1,6 @@
 package templates.components
 
+import me.mpasa.Router
 import scalatags.Text.all._
 
 object SocialT {
@@ -10,6 +11,7 @@ object SocialT {
     a(cls := "mail", href := s"mailto:miguel.perez.pasalodos@gmail.com")(icon("fas fa-envelope")),
     a(cls := "twitter", href := "https://twitter.com/Kamugo")(icon(("fab fa-twitter"))),
     a(cls := "github", href := "https://github.com/mpasa")(icon(("fab fa-github"))),
-    a(cls := "linkedin", href := "https://www.linkedin.com/in/miguel-perez-pasalodos")(icon(("fab fa-linkedin")))
+    a(cls := "linkedin", href := "https://www.linkedin.com/in/miguel-perez-pasalodos")(icon(("fab fa-linkedin"))),
+    a(cls := "rss", href := Router.Reverse.feed)(icon(("fas fa-rss")))
   )
 }
