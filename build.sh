ea07b3863a16deaeecc6baba722d4451464e9028
Miguel Pérez Pasalodos <miguel.perez.pasalodos@gmail.com>
2025-04-17 08:25:13 +0200

    Add Kubernetes deployment

--- build.sh
+++ build.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
+# Build depdendencies
 yarn install
 node_modules/.bin/webpack
 mkdir -p src/main/resources/public/styles
 sass --style compressed src/main/resources/styles:src/main/resources/public/styles
+
+# Build docker image
 sbt clean docker:publishLocal
+
+# Tag locally
 docker save -o "target/docker/mpasa.tar" "mpasa:0.1.0"
 
-echo "Done. You can restart the docker container with the new image" 
+# Load into microk8s and restart deployment
+microk8s ctr image import target/docker/mpasa.tar --digests=true
+microk8s kubectl -n mpasa set image deployment/mpasa mpasa=mpasa:0.1.0
+microk8s kubectl -n mpasa rollout restart deployment/mpasa
+
+echo "Done. Deployment is restarting..."
