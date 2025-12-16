f23042a080ff904cb938fff9b44236896ee6f4a6
Miguel Pérez Pasalodos <miguel.perez.pasalodos@gmail.com>
2025-12-16 16:49:42 +0100

    Add GHCR image repository

--- build.sh
+++ build.sh
@@ -13,12 +13,18 @@ mkdir -p src/main/resources/public/styles
 # Build docker image
 sbt clean docker:publishLocal
 
-# Tag locally
-docker save -o "target/docker/mpasa.tar" "mpasa:0.1.0"
+# Tag locally and push image
+TAG=$(date +"%Y%m%d-%H%M")
+docker tag mpasa:0.1.0 mpasa:$TAG
+docker tag mpasa:$TAG ghcr.io/mpasa/mpasa:$TAG
+docker tag mpasa:$TAG ghcr.io/mpasa/mpasa:latest
+docker push ghcr.io/mpasa/mpasa:$TAG
+docker push ghcr.io/mpasa/mpasa:latest
 
-# Load into microk8s and restart deployment
+# Push image and restart deployment
 microk8s ctr image import target/docker/mpasa.tar --digests=true
 microk8s kubectl -n mpasa set image deployment/mpasa mpasa=mpasa:0.1.0
 microk8s kubectl -n mpasa rollout restart deployment/mpasa
+microk8s kubectl -n mpasa set image deployment/mpasa mpasa=ghcr.io/mpasa/mpasa:$TAG
 
 echo "Done. Deployment is restarting..."
